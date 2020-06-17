c7bb1ab79c148489cd9d6304ce46611022828e87
Amanda Tomlinson <axtomlinson@gmail.com>
2020-06-16 21:23:53 -0700

    added url to core and detail http request

--- core/views.py
+++ core/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tank
 
-def detail(request, aquarium_id, aquarium_nickname):
-    return HttpResponse("Info on %s." % aquarium_nickname)
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, aquarium_id):
+    try:
+        t = Tank.objects.get(pk=aquarium_id)
+    except:
+        return HttpResponse("Tank doesn't exist")
+    return HttpResponse("Info on %s." % t.nickname)
