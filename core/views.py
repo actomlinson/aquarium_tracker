004ec16b072c4a75f2d1120d4bda9afa073e3fa6
Amanda Tomlinson <axtomlinson@gmail.com>
2020-06-28 18:53:19 -0700

    settings:  added static dir; urls,views,html modifying aqaurium detail view

--- core/views.py
+++ core/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Aquarium
+from django.views import generic
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, aquarium_id):
     aquarium = get_object_or_404(Aquarium, pk=aquarium_id)
-    return HttpResponse("Info on %s." % aquarium.nickname)
+    return render(request, 'aquarium_detail.html', {'aquarium': aquarium})
 
-#def detail(request, question_id):
-    #try:
-        #question = Question.objects.get(pk=question_id)
-    #except Question.DoesNotExist:
-        #raise Http404("Question does not exist")
-    #return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Aquarium
+    template_name = 'aquarium_detail.html'
+
+
